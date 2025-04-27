09cc174dc98c0160b1bfa162dabdc7e59aa2815e
Bay <28522740+zX3no@users.noreply.github.com>
2025-04-27 12:39:06 +0930

    added avsetmmthreadcharacteristics to change audio thread priority. it works!

--- src/other.rs
+++ src/other.rs
@@ -7,6 +7,7 @@ pub struct SecurityAttributes {
     pub bInheritHandle: i32,
 }
 
+#[link(name = "Avrt")]
 unsafe extern "system" {
     pub fn CreateEventA(
         lpEventAttributes: *mut SecurityAttributes,
@@ -15,6 +16,30 @@ unsafe extern "system" {
         lpName: *const i8,
     ) -> *mut c_void;
     pub fn WaitForSingleObject(hHandle: *mut c_void, dwMilliseconds: u32) -> u32;
+    pub fn AvSetMmThreadCharacteristicsA(TaskName: *const i8, TaskIndex: *mut u32) -> *mut c_void;
+    pub fn GetLastError() -> u32;
+}
+
+pub const ERROR_INVALID_TASK_NAME: u32 = 1550;
+pub const ERROR_INVALID_TASK_INDEX: u32 = 1551;
+pub const ERROR_PRIVILEGE_NOT_HELD: u32 = 1314;
+
+pub fn set_pro_audio_thread() -> u32 {
+    unsafe {
+        let mut task_index = 0;
+        if AvSetMmThreadCharacteristicsA("Pro Audio\0".as_ptr() as *const i8, &mut task_index)
+            .is_null()
+        {
+            match GetLastError() {
+                ERROR_INVALID_TASK_NAME => panic!("Invalid task name"),
+                ERROR_INVALID_TASK_INDEX => panic!("Invalid task index"),
+                ERROR_PRIVILEGE_NOT_HELD => panic!("Required privilege not held"),
+                _ => unreachable!(),
+            }
+        }
+        task_index
+    }
 }
 
 pub const WAIT_OBJECT_0: u32 = 0;
+pub const WAIT_TIMEOUT: u32 = 258;
